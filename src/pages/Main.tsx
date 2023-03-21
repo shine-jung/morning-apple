b650d5e07b87e52e4b4721fa7915de2b5f3b05fc
Ko Seokjun <ksj@Koui-noteubug.local>
2023-03-21 09:26:41 +0900

    [Add] animation code

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,14 +7,50 @@ import { IPost } from '../types/post';
 export default function Main() {
   const [posts, setPosts] = useState<IPost[]>([]);
   const post = posts[Math.floor(Math.random() * posts.length)];
+  const [name, setName] = useState("기본")
 
   const loadData = async () => {
     const response = await getPosts();
     setPosts(response);
   };
 
+  const changeName = () => {
+    const namesList = [
+      'Marcelo',
+      'Lizzette',
+      'Pauline',
+      'Fumiko',
+      'Tomasa',
+      'Bertha',
+      'Antoinette',
+      'Tianna',
+      'Ammie',
+      'Victorina',
+      'Marlon',
+      'Jules',
+      'Arletha',
+      'Ellyn',
+      'Karol',
+      'Corrin',
+      'Josephine',
+    ];
+    const choice = namesList[Math.floor(Math.random() * namesList.length)];
+    return choice;
+  }
+  const fixName = () => {
+    const n = changeName();
+    setName(n);
+  }
+
   useEffect(() => {
     loadData();
+    let timer = setInterval(()=> {
+      fixName();
+    }, 25);
+    setTimeout(()=>{
+      clearInterval(timer);
+      setName(post?.content)
+    }, 1000);
   }, []);
 
   return (
@@ -33,7 +69,10 @@ export default function Main() {
       {post?.imageURL && (
         <Box component="img" src={post?.imageURL} sx={{ height: 400, borderRadius: 1 }} />
       )}
-      <Typography variant="h4">{post?.content}</Typography>
+      <Typography variant="h4">
+        {post?.content}
+        {name}
+      </Typography>
       <MuiLink component={Link} to="/post">
         글 쓰러 가기
       </MuiLink>
